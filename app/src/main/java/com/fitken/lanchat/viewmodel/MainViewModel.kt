97df05546b9e0b2b454ffb039762ff5a705284e2
Hung Nguyen <hung.nguyen@saigontechnology.vn>
2018-01-05 11:34:59 +0700

    no message

--- app/src/main/java/com/fitken/lanchat/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/fitken/lanchat/viewmodel/MainViewModel.kt
@@ -1,5 +1,12 @@
 package com.fitken.lanchat.viewmodel
 
+import android.arch.lifecycle.AndroidViewModel
+import com.fitken.lanchat.MyApplication
+
 /**
  * Created by ken on 12/27/17.
  */
+
+class MainViewModel : AndroidViewModel( MyApplication.instance){
+
+}
